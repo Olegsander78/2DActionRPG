334fe695fa57f9417498b120f8e055b82baf5d12
Oleg Kotov <onkotov@gmail.com>
2022-07-20 01:28:05 +0500

    Создал класс Character

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Character : MonoBehaviour, IDamagable
+{
+    public enum Team
+    {
+        Player,
+        Enemy
+    }
+
+    public string DisplayName;
+    public int CurHP;
+    public int MaxHP;
+
+    [SerializeField] private Team team;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip hitSFX;
+
+    public event UnityAction onTakeDamage;
+    public event UnityAction onHeal;
+
+    public void TakeDamage(int damageToTake)
+    {
+        CurHP -= damageToTake;
+        audioSource.PlayOneShot(hitSFX);
+
+        onTakeDamage?.Invoke();
+
+        if (CurHP <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+        
+    }
+
+    public Team GetTeam()
+    {
+        return team; 
+    }
+
+    public void Heal(int healAmount)
+    {
+        CurHP += healAmount;
+
+        if(CurHP>MaxHP)
+            CurHP = MaxHP;
+
+        onHeal?.Invoke();
+    }
+}
